9b1cc73f7b29bf3969b8e8fab23601c4037b99ec
mrSergey <you@example.com>
2020-10-13 22:29:46 +0300

    add about  comparison variables

---
+++ Variables/Variables/signed_unsigned.cpp
@@ -0,0 +1,45 @@
+﻿// Сравнение знаковых и беззнаковых переменных
+
+#include <iostream>
+
+void signedVariables()
+{
+	int x = 0x1;			// 1
+	int y = 0xffffffff;		// -1
+
+	if (x > y)				// true, т.к. 1 > -1
+	{
+		std::cout << "(signed) " << x << " больше " << y << std::endl;
+	}
+	else
+	{
+		std::cout << "(signed) " << x << " меньше " << y << std::endl;
+	}
+
+}
+
+void unsignedVariables()
+{
+	unsigned int x = 0x1;				// 1
+	unsigned int y = 0xffffffff;		// -1
+
+	if (x > y)							// false т.к.  1 < 4294967295
+	{
+		std::cout << "(unsigned) " << x << " больше " << y << std::endl;
+	}
+	else
+	{
+		std::cout << "(unsigned) " << x << " меньше " << y << std::endl;
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	signedVariables();
+	unsignedVariables();
+
+	return 0;
+}
