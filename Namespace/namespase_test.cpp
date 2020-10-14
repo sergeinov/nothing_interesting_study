167d5507bac283109e6e7bdae08b4513a057ba15
mrSergey <you@example.com>
2020-10-14 12:56:05 +0300

    fix namespace

--- Namespace/namespase_test.cpp
+++ Namespace/namespase_test.cpp
@@ -10,13 +10,27 @@ namespace Second
 {
 	int version = 5;
 }
+// использование директивы using
+using namespace Second;
+
+namespace Third
+{
+	int version = 9;
+}
+// использование псевдонима
+namespace ver = Third;
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
 	std::cout << First::version << std::endl;
-	std::cout << Second::version << std::endl;
+	// появляется возможность использовать пространство Second без оператора ::
+	std::cout << version << std::endl;
+
+	std::cout << "----------------" << std::endl;
+
+	std::cout << ver::version << std::endl;
 
 	return 0;
 }
