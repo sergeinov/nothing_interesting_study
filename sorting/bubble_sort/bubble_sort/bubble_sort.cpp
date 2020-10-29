5f9f6bea940faf363f82acc722ce89c9f82665f3
SergeyAN <lookbugs@mail.ru>
2020-10-29 11:27:09 +0300

    add bubble_sort

---
+++ sorting/bubble_sort/bubble_sort/bubble_sort.cpp
@@ -0,0 +1,35 @@
+//  пример пузырьковой сортировкина с++
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int arr[] = {7, 2, -5, 6, 0, 3, 2, 5, 3, -9, 99, 100};  // массив чисел
+    int sizeArr = sizeof(arr) / sizeof(int);  // размер массива
+
+    // проходим по всему массиву
+    for (size_t i = 0; i < sizeArr; i++)
+    {
+        // поиск из оставшихся неупорядоченных чисел
+        for (size_t j = 0; j < sizeArr - 1; j++)
+        {
+            if (arr[j + 1] < arr[j])
+            {
+            // обмен местами значения текущего и минимального элементов
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            }
+        }
+  
+        // вывод массива
+        std::cout << i+1 << " круг: \t";
+        for (auto& i : arr)
+        {
+            std::cout << i << ",";
+        }
+        std::cout << std::endl;
+    }
+}
