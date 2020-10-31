19c83ba09cf8b6bef6b6f862158285dad7a91c31
SergeyAN <lookbugs@mail.ru>
2020-10-31 13:04:43 +0300

    fix bubble_sort

--- sorting/bubble_sort/bubble_sort/bubble_sort.cpp
+++ sorting/bubble_sort/bubble_sort/bubble_sort.cpp
@@ -32,4 +32,28 @@ int main()
         }
         std::cout << std::endl;
     }
+    // второй вариант без лишней сортировки
+    /*
+     for (size_t i = 0; i < sizeArr; i++)
+    {
+    bool flag = false;
+        // поиск из оставшихся неупорядоченных чисел
+        for (size_t j = 0; j < sizeArr- 1; j++)
+        {
+            if (arr[j + 1] < arr[j])
+            {
+            // обмен местами значения текущего и минимального элементов
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+
+            flag = true;
+            }
+        }
+        if (!flag)
+        {
+            break;
+        }
+    }
+    */
 }
