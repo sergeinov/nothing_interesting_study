4b609b939452d0782e9f60dc071d918ecdce003a
SergeyAN <lookbugs@mail.ru>
2020-11-26 18:38:44 +0300

    add Lab2 C110

---
+++ Practice_DEV-С110/Practice_2/Lab2/Lab2/other.h
@@ -0,0 +1,10 @@
+
+//Прототипы функций, используемых в данном задании :
+//void VarArgs(int arg1, ...);
+
+void Sort(char* pcFirst, int nNumber, int size,
+void(*Swap)(void*, void*), int(*Compare)(void*, void*));
+void SwapInt(void* p1, void* p2);
+int CmpInt(void* p1, void* p2);
+
+
