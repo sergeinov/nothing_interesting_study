707aaf29134b3e95c1d23b73ee85ff46d9670607
SergeyAN <lookbugs@mail.ru>
2020-12-08 13:25:19 +0300

    add changes lab3 C110

--- Practice_DEV-С110/Practice_3/Lab3/Lab3/book.cpp
+++ Practice_DEV-С110/Practice_3/Lab3/Lab3/book.cpp
@@ -5,7 +5,7 @@
 
 
 // для Задания 1.3
-void printBOOK(const BOOK* book) 
+void printBOOK2(const BOOK2* book) 
 {
 	printf("Автор : %s\nНазвание : %s\nГод : %d\nЦена : %.2f\nКатегория : %d",
 			book->autor, book->title, book->year, book->price,
@@ -16,29 +16,113 @@ void printBOOK(const BOOK* book)
 
 
 // для Задания 1.4
-void editBook(BOOK* book) 
+void editBook2(BOOK2* book) 
 {
-	bool flag = true;
+	bool flag = true;	// для завершения цикла
 	while (flag) 
 	{
-		printf("Введите автора, название книги, год, цену, категорию(1,2,3,4,5,6,7):\n");
-		scanf("%49s %99s %d %f %d", book->autor, book->title, &book->year, &book->price, &book->category);
+		printf("\nВведите автора:\n");
+		scanf("%49s", book->autor);
 
-		if (book->year < 1445)
+		printf("\nВведите название книги:\n");
+		scanf("%99s", book->title);
+
+		printf("\nВведите год:\n");
+		scanf(" %d", &book->year);
+		if (book->year < 1445 || book->year > 2020)
 		{
 			printf("Не допустимый год издания.Не меньше 1445г\n");
 			continue;
 		}
-		else if (book->price < 0)
+
+		printf("\nВведите цену:\n");
+		scanf("%f", &book->price);
+		if (book->price < 0)
 		{
 			printf("Цена не может быть отрицательной!\n");
 			continue;
-		} 
-		else if (book->category < 0 || book->category > 6)
+		}
+
+		printf("\nВведите категорию(1,2,3,4,5,6,7):\n");
+		scanf("%f", &book->category);
+		/*if (book->category < 0 || book->category > 6)
 		{
 			printf("Категория не верна!\n");
-		}
+			continue;
+		}*/
+
 		flag = false;
 	}
 	
+};
+
+////////////////////////////////////////////////
+
+// для Задания 2
+
+//функция вывода меню
+void outputMenu()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6 };
+	printf("Меню команд:\n%d - Распечатать картотеку книг. \n%d - Добавить новую книгу.\n\
+%d - Удалить книгу из картотеки.\n%d - Записать содежимое картотеки в файл\n\
+%d - Считать из файла содержимое в картотеку.\n%d - Выход из программы", \
+			arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+};
+
+// функция для распечатки картотеки
+/*void printBOOK(const CARD_INDEX* pCard, int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		pCard[i] = new CARD_INDEX;
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Автор : %s\nНазвание : %s\nГод : %d\nЦена : %.2f\nКатегория : %d",
+			pCard->pB[i], book[i].title, book[i].year, book[i].price, book[i].category);
+	}
+};*/
+
+// функция добавления книги
+void addBook(CARD_INDEX* pCard)
+{
+	if (pCard->count >= pCard->cap)		// емкость исчерпана
+	{
+		// перераспределяем память
+	}
+	bool flag = true;	// для завершения цикла
+	while (flag)
+	{
+		printf("\nВведите автора:\n");
+		//scanf("%49s", pCard->pB);
+
+		printf("\nВведите название книги:\n");
+		//scanf("%99s", book[quantity].title);
+
+		printf("\nВведите год:\n");
+		//scanf(" %d", &book[quantity].year);
+		//if (book[quantity].year < 1445 || book[quantity].year > 2020)
+		//{
+		//	printf("Не допустимый год издания.Не меньше 1445г\n");
+		//	continue;
+		//}
+
+		printf("\nВведите цену:\n");
+		//scanf("%f", &book[quantity].price);
+		//if (book[quantity].price < 0)
+		//{
+		//	printf("Цена не может быть отрицательной!\n");
+		//	continue;
+		//}
+
+		//printf("\nВведите категорию(1,2,3,4,5,6,7):\n");
+		//scanf("%f", &book[quantity].category);
+		//if (book->category < 0 || book->category > 6)
+		//{
+		//	printf("Категория не верна!\n");
+		//	continue;
+	}
+
+		flag = false;
 };
--- Practice_DEV-С110/Practice_3/Lab3/Lab3/book.h
+++ Practice_DEV-С110/Practice_3/Lab3/Lab3/book.h
@@ -1,20 +1,51 @@
 #pragma once
 
-enum CATEGORY { Боевики = 1, Детективы, Приключения, Фантастика, Fantasy, История, Спорт };
+enum CATEGORY2 { Боевики = 1, Детективы, Приключения, Фантастика, Фэнтези, История, Спорт };
 
-struct BOOK
+struct BOOK2
 {
 	char autor[50] = "Сергей Семонович Труш";
 	char title[100] = "Трясина";
-	int year = 2011;
+	unsigned short year = 2011;
 	float price = 55.45;
-	CATEGORY category = Fantasy;
+	CATEGORY2 category = Фантастика;
 };
 
 
 // для Задания 1.3
-void printBOOK(const BOOK* book);
+void printBOOK2(const BOOK2* book);
 
 
 // для Задания 1.4
-void editBook(BOOK* book);
+void editBook2(BOOK2* book);
+
+////////////////////////////////////////////////
+
+// для Задания 2
+
+enum eCATEGORY { Detectives = 1, Adventure, Fantastic, Fantasy, History, Sports };
+
+struct BOOK
+{
+	char autor[50] = {0};
+	char title[100] = {0};
+	unsigned short year;
+	float price;
+	eCATEGORY category;
+};
+
+struct CARD_INDEX
+{
+	BOOK** pB;				// указатель на картотеку BOOK* pB
+	size_t count = 0;		// количество книг
+	size_t cap;				// емкость картотеки
+};
+
+// функция вывода меню
+void outputMenu();
+
+// функция для распечатки картотеки
+void printBOOK(const CARD_INDEX* pCard, int size);
+
+// функция добавления книги
+void addBook(CARD_INDEX* pCard);
