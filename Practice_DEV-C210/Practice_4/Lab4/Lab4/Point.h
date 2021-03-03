90aabe6eee4c144eadba9cf6e60122cad2105998
SergeyAN <lookbugs@mail.ru>
2021-03-03 14:32:47 +0300

    change lab4 C210

---
+++ Practice_DEV-C210/Practice_4/Lab4/Lab4/Point.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<iostream>
+class Point
+{
+public:
+
+	Point();
+	Point(int x, int y);
+	//~Point();
+
+	// перегрузка оператора==
+	bool operator==(const Point& point) const;
+	bool operator<(const Point& point) const;
+
+	friend std::ostream& operator<<(std::ostream& os, const Point& place); // вывод координат
+	friend std::ostream& operator<<(std::ostream& os, const Point* place); // вывод координат
+	friend bool NegPoint(const Point& object);
+private:
+	int m_x;
+	int m_y;
+};
+
---
+++ Practice_DEV-C210/Practice_4/Lab4/Lab4/template.h
@@ -0,0 +1,47 @@
+#pragma once
+#include<iostream>
+#include <queue>
+#include<deque>
+
+template<typename T> const typename T::value_type& topAll(const T& adapter)
+{
+	return adapter.top();
+}
+
+template<typename T> const T& topAll(const std::queue<T>& adapter)
+{
+	return adapter.front();
+}
+
+template<typename A>void PrintAdapter(A a)
+{
+	if ( a.empty() )
+	{
+		std::cout << "\nEmpty!!!" << std::endl;
+	}
+
+	while ( !a.empty() )
+	{
+		std::cout << topAll(a) << " ";
+
+		a.pop();
+	}
+	
+}
+
+
+// распечатать любой контейнер
+template<typename T> void PrintAll(const T& data)
+{
+	typename T::const_iterator it;										// переменная итератор
+	//typename T::value_type type = typename T::value_type();					// тип контейнера
+	std::cout << "\n" << typeid( T ).name() << std::endl;						// вывод типа контейнера
+	std::cout << typeid( typename T::value_type ).name() << std::endl;	//  вывод  имени контейнера
+
+	for ( it = data.begin(); it != data.end(); it++ )
+	{
+		std::cout << *it << " ";										// распечатываем
+	}
+
+	std::cout << std::endl;
+}
