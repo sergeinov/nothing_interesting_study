77183b5834dd06e9ef2e7e584e16033d6b3d5c41
SergeyAN <lookbugs@mail.ru>
2021-03-26 11:40:24 +0300

    initial Lab2 C220

---
+++ Practice_DEV-C220/Practice_2/Lab2/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+#include<iterator>
+
+#include <iostream>
+
+int main()
+{
+   
+
+
+
+}
+
+
